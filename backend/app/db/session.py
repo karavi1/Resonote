3e0c79e6f43f9faf51a650d8d75181902f3792b7
Kaushik Ravi <kaushikravi@Mac.attlocal.net>
2025-07-24 14:48:09 -0700

    Backend set up for prod deployment on Render

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,9 +1,10 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
-load_dotenv()
+if os.getenv("FLASK_ENV") != "production":
+    from dotenv import load_dotenv
+    load_dotenv()
 
 user = os.getenv("DATABASE_USER")
 password = os.getenv("DATABASE_PASSWORD")
@@ -12,6 +13,5 @@ port = os.getenv("DATABASE_PORT", "3306")
 db_name = os.getenv("DATABASE_NAME")
 
 DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}"
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
 from app.routes.core_routes import core_bp
-from dotenv import load_dotenv
+import os
+
+if os.getenv("FLASK_ENV") != "production":
+    from dotenv import load_dotenv
+    load_dotenv()
 
 def create_app():
-    load_dotenv()
     app = Flask(__name__)
     CORS(app)
-    app.config.from_pyfile("config.py")
+
+    config_path = os.getenv("FLASK_CONFIG", "config.py")
+    app.config.from_pyfile(config_path)
 
     app.register_blueprint(core_bp, url_prefix="/api")
+
     for rule in app.url_map.iter_rules():
         print(f"{rule.methods} -> {rule.rule}")
 
